6376890ea0872c046ef342450ffb8923bed9b384
SomeshPurwar <somesh.purwar20@gmail.com>
2024-08-11 17:07:43 +0530

    commit

--- client/src/components/NotFound.jsx
+++ client/src/components/NotFound.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='position-relative' style={{minHeight: 'calc(100vh - 70px)'}}>
+      <h2 className='position-absolute text-secondary' style={{top:'50%',left:'50%', transform:'translate(-50%,-50%)'}}>404 | Not Found.</h2>
+      
+    </div>
   )
 }
 
